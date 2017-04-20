a14434fa2987f522add41862e0746f3b185366a9
Brian OToole <botoole518@gmail.com>
2017-04-20 17:08:06 +0000

    added scss assets

--- src/App.vue
+++ src/App.vue
@@ -24,19 +24,22 @@ export default {
 </script>
 
 <style lang="sass">
-@import './assets/main.scss';
-
+@import './assets/app.scss';
 .header {
   width: 100%;
+  margin: 0;
+  padding: calc-em(20px) 0;
   background: $black;
+  border-bottom: 1px solid $primary;
 }
 .nav {
   li {
     display: inline-block;
   }
   li a {
-    padding: 5px;
+    padding: calc-em(10px);
     color: $secondary;
+    text-decoration: none;
   }
 }
 </style>
